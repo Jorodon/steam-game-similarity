5867c91c402497fbe2661eae32c7f1ff422b931a
Colbygrimmscott <colbygrimmscott@ufl.edu>
2025-11-18 15:24:30 -0500

    Added a loop and brute force algorithm for comparison
    Added a brute force nearest neighbor search algorithm using sklearn to test my implementation for accuracy. Also added while loop to run many queries in succession. Added additional timer to track traversal time for forest and query time for brute force method.

--- rp_forest_main.py
+++ rp_forest_main.py
@@ -12,7 +12,7 @@ def main():
 
     #Loads preprocessed data and creates a RPForest using it
     initial_data = load_data_test.load_preprocessed_data()
-    rp_forest = RPForest(initial_data, 5, 17, 15)
+    rp_forest = RPForest(initial_data, 50, 17, 15)
     rp_forest.createForest()
 
     #Tracks ending time of forest creation and prints result
@@ -20,17 +20,39 @@ def main():
     time_passed = forest_create_end - forest_create_start
     print(f"Time to create forest is {time_passed}")
 
-    test_index = random.randint(0, 111452)
-    test_index = 31341
-    data_indices = rp_forest.traverseForest(test_index, 10)
-    #print(f"Test Index: {test_index}\nData Count: {len(data_indices)}\nData Indices: {data_indices}")
+    count = 0
+    while count < 10:
+        test_index = random.randint(0, 111452)
+        #test_index = 31341
 
-    #Loading the metadata to display the game names
-    metadata_dict = load_data_test.load_metadata()
-    search_name = metadata_dict.get(str(test_index)).get("Name")
-    game_names = [metadata_dict.get(str(key)).get("Name") for key in data_indices]
-    print(f"Data Count: {len(data_indices)}\nGame Searched: {search_name}\nGames: {game_names}")
+        #Tracks time to query the forest
+        forest_traverse_start = time.time()
 
+        #Finds k nearest neighbors in forest
+        data_indices = rp_forest.traverseForest(test_index, 10)
+
+        #Tracks ending time of query and prints results
+        forest_traverse_end = time.time()
+        time_passed = forest_traverse_end - forest_traverse_start
+        print(f"Time to query forest is {time_passed}")
+        #print(f"Test Index: {test_index}\nData Count: {len(data_indices)}\nData Indices: {data_indices}")
+
+        #Loading the metadata to display the game names
+        metadata_dict = load_data_test.load_metadata()
+        search_name = metadata_dict.get(str(test_index)).get("Name")
+        game_names = [metadata_dict.get(str(key)).get("Name") for key in data_indices]
+        print(f"Data Count: {len(data_indices)}\nGame Searched: {search_name}\nGames: {game_names}")
+
+        '''TO-DO - Uses brute force method to find the k-most-similar games'''
+        nearest_neighbors = load_data_test.tuning_tree(test_index, 10)
+        
+        #Outputs brute force results
+        game_names_brute = [metadata_dict.get(str(key)).get("Name") for key in nearest_neighbors]
+        print(f"\nBrute Force Games: {game_names_brute}")
+
+        count += 1
+
+    
 
 if __name__ == "__main__":
     main()
