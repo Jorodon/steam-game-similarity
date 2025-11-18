65aaf4875f957c52e040d1d472a00522323c08ab
Colbygrimmscott <colbygrimmscott@ufl.edu>
2025-11-18 13:17:21 -0500

    Updated main() for clearer testing
    Updated main() to get names of the games from metadata and print them, which allows searching the games on Steam to determine effectiveness of algorithm.

--- rp_forest_main.py
+++ rp_forest_main.py
@@ -12,7 +12,7 @@ def main():
 
     #Loads preprocessed data and creates a RPForest using it
     initial_data = load_data_test.load_preprocessed_data()
-    rp_forest = RPForest(initial_data, 50, 17, 15)
+    rp_forest = RPForest(initial_data, 5, 17, 15)
     rp_forest.createForest()
 
     #Tracks ending time of forest creation and prints result
@@ -21,8 +21,16 @@ def main():
     print(f"Time to create forest is {time_passed}")
 
     test_index = random.randint(0, 111452)
+    test_index = 31341
     data_indices = rp_forest.traverseForest(test_index, 10)
-    print(f"Test Index: {test_index}\nData Count: {len(data_indices)}\nData Indices: {data_indices}")
+    #print(f"Test Index: {test_index}\nData Count: {len(data_indices)}\nData Indices: {data_indices}")
+
+    #Loading the metadata to display the game names
+    metadata_dict = load_data_test.load_metadata()
+    search_name = metadata_dict.get(str(test_index)).get("Name")
+    game_names = [metadata_dict.get(str(key)).get("Name") for key in data_indices]
+    print(f"Data Count: {len(data_indices)}\nGame Searched: {search_name}\nGames: {game_names}")
+
 
 if __name__ == "__main__":
     main()
