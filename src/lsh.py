bb8cbb235ad7df3f9483030c9c7b77029e259360
Jordon <57411275+Jorodon@users.noreply.github.com>
2025-11-25 13:35:06 -0700

    Update lsh.py
    Started the build function that creates an empty LSH without filling the values

--- src/lsh.py
+++ src/lsh.py
@@ -21,12 +21,28 @@ class LSH:
         #keys = hash code, values = list of indices from _dataSet
         self._tables = None
 
+    #Function that builds LSH tables from normalized dataset
+    def build(self, dataSet: np.ndarray) -> None:
+        
+        #grabs number of entries and the vector dimension from the dataset shape
+        numGames = dataSet.shape[0]
+        dims = dataSet.shape[1]
+
+        #Sets class variable _dataSet to called dataset
+        self._dataSet = dataSet
+
+        #Creates 3D numpy array of floats -> _hyperplanes[table][hyperplane in table][floats for given hyperplane]
+        self._hyperplanes = np.random.normal(size = (self._hashTables, self._numPlanes, dims))
+
+        #Creates an empty list of dictionaries for each table
+        self._tables = [dict() for t in range(self._hashTables)]
+
 #Test/Debug area
 if __name__ == "__main__":
     test = load_numpy_preprocessed_data()
     meta = load_metadata()
 
     print("LSH file wired up")
-    print("Matrix shape:", test.shape)
+    print("Matrix shape:", test.shape[0])
     print("First row norm:", np.linalg.norm(test[0]))
     print("Example game:", meta["0"]["Name"])
