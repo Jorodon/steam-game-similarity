db813853514690a609b42667a0b5428e80c1c1e0
Colbygrimmscott <colbygrimmscott@ufl.edu>
2025-11-26 12:59:42 -0500

    Added comments to createForest() and helper functions
    Added comments missing from three functions - createForest(), globalDataset(), and createTreeGlobal().

--- rp_forest.py
+++ rp_forest.py
@@ -3,11 +3,12 @@ import random
 import time
 import multiprocessing as mp
 
+#Takes in dataset and makes it a global variable (prevents multiple copies being created for each worker process)
 def globalDataset(dataset):
     global multiprocessor_dataset
     multiprocessor_dataset = dataset
 
-#Non-class function allows multiprocessing to execute, parallelizes the creation of 
+#Non-class function allows multiprocessing to execute, parallelizes the creation of trees
 def createTreeGlobal(args):
 
     #Creates a tree 
@@ -37,16 +38,21 @@ class RPForest:
         self._max_level = max_level
         self._trees = []
 
-    #Creates an RP forest by creating many individual RP trees
+    #Creates an RP forest by creating many individual RP trees (using multi-processing)
     def createForest(self):
         
+        #Creates a list of tasks to be divided up among cores 
+        #Consists of function inputs for RPTree createTree()
         multi_task_list = []
         for i in range(self._num_of_trees):
             multi_task_list.append([self._max_level, self._min_leaf_size, i])
         
+        #Creates a multiprocessing pool to manage pool of worker processes
         with mp.Pool(initializer=globalDataset, initargs=(self._dataset,)) as pool:
+            #Maps the list of tasks to the creareTreeGlobal function, returns list of tree roots
             forest_roots = pool.map(createTreeGlobal, multi_task_list)
 
+        #Turns list of tree roots into list of RPTree objects (setting the root for each)
         for root in forest_roots:
             rp_tree = RPTree(self._dataset, self._max_level, self._min_leaf_size)
             rp_tree._root = root
