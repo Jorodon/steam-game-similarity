df7fec2f7129057c2e3e1b6bb2d1c9aed3194a75
Jordon <57411275+Jorodon@users.noreply.github.com>
2025-11-30 17:21:51 -0500

    Added front page to GUI
    Added method dropdown, game searchbox, and similar game sliders

--- src/gui.py
+++ src/gui.py
@@ -2,19 +2,33 @@ import time
 import streamlit as st
 from guiHelpers import initRPForest, initLSH, initMetadata, indexFromName
 
-RPForest = initRPForest()
-LSH = initLSH()
-metadata = initMetadata()
+def main():
+    st.title("Steam Game Similarity")
+
+    metadata = initMetadata()
+    LSH = initLSH()
+    #RPForest = initRPForest()
+
+    #Method dropdown
+    available_methods = ["LSH"]
+    method = st.selectbox("Method", available_methods)
+
+    #Game searchbox
+    game_name = st.text_input("Game name")
+
+    #Amount of similar games slider
+    k = st.slider("Number of similar games", 5, 30, 10)
+
+    
 
 #To Do:
-'''
-- Dropdown for RP Forest, LSH, or both
-- Random game name or search
-- Stats
-'''
+#- Dropdown for RP Forest, LSH, or both
+#- Random game name or search
+#- Stats
+
 
 if __name__ == "__main__":
-    print(indexFromName("BrainTeaser", metadata))
+    main()
 
 
 
